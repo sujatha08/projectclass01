df215ffd88fa7346f565cffc467fadb12df29e8b
sujatha08 <sujasenthil1999@gmail.com>
2026-04-18 20:40:21 +0530

    Added methods from vaishnavi

--- src/test/java/com/samplegit/ClassA.java
+++ src/test/java/com/samplegit/ClassA.java
@@ -13,4 +13,16 @@ public class ClassA {
 	{
 		System.out.println("METHOD 3");
 	}
+	public void a()
+	{
+		System.out.println("a");
+	}
+	public void b()
+	{
+		System.out.println("b");
+	}
+	public void c()
+	{
+		System.out.println("c");
+	}
 }
